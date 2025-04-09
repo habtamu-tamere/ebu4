b6d1604ae0e73acb583f70578da93580c7860944
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-04-09 18:21:08 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAEzJ7GpEjLnFldkMaDO8yHagwQ654490Q",
+    authDomain: "ebook-d2d5d.firebaseapp.com",
+    projectId: "ebook-d2d5d",
+    storageBucket: "ebook-d2d5d.firebasestorage.app",
+    messagingSenderId: "326709138585",
+    appId: "1:326709138585:web:3e5b4f6c8390ce00a09408",
+    measurementId: "G-2Y2GTFZ2FQ"
+    // apiKey: "YOUR_API_KEY",
+    // authDomain: "YOUR_AUTH_DOMAIN",
+    // databaseURL: "YOUR_DATABASE_URL",
+    // projectId: "YOUR_PROJECT_ID",
+    // storageBucket: "YOUR_STORAGE_BUCKET",
+    // messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    // appId: "YOUR_APP_ID",
+    // measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
